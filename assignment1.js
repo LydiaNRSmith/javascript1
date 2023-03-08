5e8dd7deb1983148adde3e4fa1ff2d8216b2c842
LydiaNRSmith <127160073+LydiaNRSmith@users.noreply.github.com>
2023-03-08 16:39:46 -0700

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,5 @@
+var username;
+username = prompt("Please enter your name: ");
+
+alert("Hello, " + username + " your quote of the day is: The only impossible journey is the one you never begin.");
+
